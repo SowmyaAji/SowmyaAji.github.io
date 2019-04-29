5d51b452b89f24dce16d3a4634b05a56c5eb75cf
Sowmya <sowmya.aji@gmail.com>
2019-04-29 19:09:16 -0400

    Add JS solution to overscrolling

--- js/main.js
+++ js/main.js
@@ -1,3 +1,26 @@
+$(document).on('touchmove', function (e) {
+    e.preventDefault();
+});
+//uses body because jquery on events are called off of the element they are
+//added to, so bubbling would not work if we used document instead.
+$('body').on('touchstart', '.scrollable', function (e) {
+    if (e.currentTarget.scrollTop === 0) {
+        e.currentTarget.scrollTop = 1;
+    } else if (e.currentTarget.scrollHeight === e.currentTarget.scrollTop + e.currentTarget.offsetHeight) {
+        e.currentTarget.scrollTop -= 1;
+    }
+});
+//prevents preventDefault from being called on document if it sees a scrollable div
+$('body').on('touchmove', '.scrollable', function (e) {
+    e.stopPropagation();
+});
+
+
+
+
+
+
+
 $(".custom").on('click', function () {
     $("#name").hide();
 })
