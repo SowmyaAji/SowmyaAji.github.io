f51a642fae2d8dcfc38fe2644cf3977168d82440
Sowmya <sowmya.aji@gmail.com>
2020-07-02 14:01:56 -0400

    Add changing background image

--- js/main.js
+++ js/main.js
@@ -21,3 +21,22 @@ $('#codeModal1').click(function () {
             }
         });
 });
+
+var images = [
+    'https://placeholdit.imgix.net/~text?txtsize=33&txt=one&w=100&h=100',
+    'https://placeholdit.imgix.net/~text?txtsize=33&txt=two&w=100&h=100',
+    'https://placeholdit.imgix.net/~text?txtsize=33&txt=three&w=100&h=100',
+    'https://placeholdit.imgix.net/~text?txtsize=33&txt=four&w=100&h=100'
+];
+
+const imgArray = ['../images/Paypal.jpg', '../images/back2.jpg', '../images/paypal1.jpeg', '../images/bulls2.jpg'];
+
+let i = 0;
+const changeImage = setInterval(
+    function () {
+        $('body').css('background-image', "url('" + imgArray[i] + "')");
+        i++;
+        if (imgArray.length == i) {
+            i = 0;
+        }
+    }, 20000)
