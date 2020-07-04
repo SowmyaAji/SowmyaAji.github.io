e2deeed6264b3738cca28720bbd0c6ee2071a5cb
Sowmya <sowmya.aji@gmail.com>
2020-07-04 19:13:42 -0400

    Add media reviews to novels modal

--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
+// hide and show the text in the index page alternating with modals.
 $(".custom").on('click', function () {
     $("#name").hide();
 })
-
 $(".close-button, .reveal-overlay").on('click', function () {
     $("#name").show();
 })
 
-
-
-
+// use foundation.js to generate modals
 $(document).foundation();
-
 $('#codeModal1').click(function () {
     $.ajax
         ({
@@ -22,6 +19,7 @@ $('#codeModal1').click(function () {
         });
 });
 
+// used for the images slider in the getJustice app details, built with foundation.js
 var images = [
     'https://placeholdit.imgix.net/~text?txtsize=33&txt=one&w=100&h=100',
     'https://placeholdit.imgix.net/~text?txtsize=33&txt=two&w=100&h=100',
@@ -29,9 +27,11 @@ var images = [
     'https://placeholdit.imgix.net/~text?txtsize=33&txt=four&w=100&h=100'
 ];
 
+// image array used for the background image in the index page
 const imgArray = ['../images/paypal1.jpeg', '../images/Read.jpg', '../images/side.jpg', '../images/PayPal.jpg', '../images/kathak.jpg', '../images/back2.jpg', '../images/Mask.jpg', '../images/bulls2.jpg'];
 
 let i = 0;
+
 const changeImage = setInterval(
     function () {
         $('body').css('background-image', "url('" + imgArray[i] + "')");
