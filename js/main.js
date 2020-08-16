80821317d5b4a314b3362503ce47152217a62adc
Sowmya <sowmya.aji@gmail.com>
2020-08-16 00:21:04 -0400

    Add spot the lie game and apps collage

--- js/main.js
+++ js/main.js
@@ -28,9 +28,9 @@ var images = [
 ];
 
 // image array used for the background image in the index page
-const imgArray = ['../images/mineshaft.jpg', '../images/paypal1.jpeg', '../images/web.jpeg', '../images/tree.jpg', '../images/panda.jpeg', '../images/Read.jpg', '../images/side.jpg', '../images/Sri.jpeg', '../images/forest1.jpg', '../images/PayPal.jpg', '../images/rang.jpg', '../images/back2.jpg', '../images/kathak.jpg', '../images/blacknwhite.jpg', '../images/bulls2.jpg'];
+const imgArray = ['../images/bulls2.jpg', '../images/mineshaft.jpg', '../images/paypal1.jpeg', '../images/web.jpeg', '../images/tree.jpg', '../images/panda.jpeg', '../images/Read.jpg', '../images/side.jpg', '../images/Sri.jpeg', '../images/forest1.jpg', '../images/PayPal.jpg', '../images/rang.jpg', '../images/back2.jpg', '../images/kathak.jpg', '../images/blacknwhite.jpg', '../images/apps.png'];
 
-const introText = ['gone down mineshafts.', 'a passion for code.', 'hosted web and radio shows.', 'aspired to scale trees.', 'met a rocket scientist.', 'written three novels.', 'acted in a soap opera.', 'interviewed 4 Prime Ministers', 'slept in forests.', "worked in corporate houses", "hung out with Picasso's model.", 'wandered 7 countries.', 'performed 4 dance forms', 'witnessed riots.', 'flown in helicopters.']
+const introText = ['flown in helicopters', 'gone down mineshafts.', 'a passion for code.', 'hosted web and radio shows.', 'scaled trees.', 'met a rocket scientist.', 'written three novels.', 'acted in a soap opera.', 'interviewed 4 Prime Ministers', 'slept in forests.', "worked in corporate houses.", "hung out with Picasso's model.", 'wandered 7 countries.', 'performed 4 dance forms', 'witnessed riots.', 'built 16 apps.']
 
 let i = 0;
 
@@ -45,3 +45,24 @@ const changeImage = setInterval(
         }
     }, 4000)
 
+function alertWindow() {
+    let txt;
+    let newLine = "\r\n"
+    const answer = prompt("Guess the lie! Type the line number:  " + newLine + "1. I have flown in helicopters." + newLine + "2. I have gone down mineshafts." + newLine + "3. I have a passion for code." + newLine + "4. I have hosted web and radio shows." + newLine + "5. I have scaled trees." + newLine + "6. I have met a rocket scientist." + newLine + "7. I have written three novels." + newLine + "8. I have acted in a soap opera." + newLine + "9. I have interviewed 4 Prime Ministers" + newLine + "10. I have slept in forests." + newLine + "11. I have worked in corporate houses." + newLine + "12. I have hung out with Picasso's model." + newLine + "13. I have performed 4 dance forms." + newLine + "14. I have witnessed riots." + newLine + "15. I have built 16 apps.")
+    if (answer == null || "") {
+        txt = "Hey, do play! Or check out the About me blue for clues! Or click on my name for my resume!"
+    }
+    else if (answer === "5") {
+        txt = "You got that right! Do see the About me  below for more or click on my name for my resume!"
+    }
+    else {
+        txt = "You got that wrong! Do see the About me for clues or click on my name for my resume and play again!"
+    }
+    $("#answer").html(txt);
+    setTimeout(function () {
+        if ($('#answer').length > 0) {
+            $('#answer').html("");
+        }
+    }, 7000)
+
+}
