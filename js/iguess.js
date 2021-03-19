5fe54da561b636059531271d971056d45dba0331
Sowmya <sowmya.aji@gmail.com>
2021-03-19 13:00:11 -0400

    Increase iguess size to 5000

--- js/iguess.js
+++ js/iguess.js
@@ -3,68 +3,56 @@ $("#won").hide();
 $("#cheat").hide();
 
 //global variables
-let startpoint = 0
-let endpoint = 100
-let midpoint = Math.floor((startpoint + endpoint) / 2)
-let counter = 0
+let startpoint = 0;
+let endpoint = 5000;
+let midpoint = Math.floor((startpoint + endpoint) / 2);
+let counter = 0;
 
 //first guess
 $("#yes").on("click", function () {
+  $("#guesses").val(midpoint);
 
-    $("#guesses").val(midpoint)
-
-    counter = counter + 1
-    $("#count").val(counter)
-    seventhCount()
-
-})
-
+  counter = counter + 1;
+  $("#count").val(counter);
+  seventhCount();
+});
 
 //if guess equal secret number
-$("#equals").on('click', function () {
-    $("#guesses").val() === "="
-    $("#won").show();
-})
+$("#equals").on("click", function () {
+  $("#guesses").val() === "=";
+  $("#won").show();
+});
 
 //if secret number more than guess
-$("#more").on('click', function () {
-    startpoint = midpoint
-    midpoint = Math.floor((startpoint + endpoint) / 2)
-    $("#guesses").val(midpoint)
-    counter = counter + 1
-    $("#count").val(counter)
-    seventhCount()
-
-})
+$("#more").on("click", function () {
+  startpoint = midpoint;
+  midpoint = Math.floor((startpoint + endpoint) / 2);
+  $("#guesses").val(midpoint);
+  counter = counter + 1;
+  $("#count").val(counter);
+  seventhCount();
+});
 
 //if secret number less than guess
-$("#less").on('click', function () {
-    endpoint = midpoint
-    midpoint = Math.floor((startpoint + endpoint) / 2)
-    $("#guesses").val(midpoint)
-    counter = counter + 1
-    $("#count").val(counter)
-    seventhCount()
-
-
-})
+$("#less").on("click", function () {
+  endpoint = midpoint;
+  midpoint = Math.floor((startpoint + endpoint) / 2);
+  $("#guesses").val(midpoint);
+  counter = counter + 1;
+  $("#count").val(counter);
+  seventhCount();
+});
 
 //if someone cheats
 function seventhCount() {
-    if (counter > 7) {
-        console.log(counter)
-        $("#cheat").show();
-        counter = 7
-    }
-
+  if (counter > 13) {
+    console.log(counter);
+    $("#cheat").show();
+    counter = 13;
+  }
 }
-
 
 //reload the game
 $(document).on("click", ".yes2", function () {
-    location.reload(true);
+  location.reload(true);
 });
-
-
-
-
